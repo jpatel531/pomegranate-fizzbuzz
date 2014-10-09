5e5d5e59f1c28c2fa059cf3b91506fb033c7f690
jpatel531 <jamie@notespublication.com>
2014-10-09 11:57:19 +0100

    knows when a number is not divisible by 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,8 @@ describe 'Fizzbuzz' do
 		expect(divisible_by_three? 3).to be true
 	end
 
+	it 'knows when a number is not divisible by 3' do 
+		expect(divisible_by_three? 1).to be false
+	end
+
 end
