72794c3aff770da357036d3ee96938b17f38963f
jpatel531 <jamie@notespublication.com>
2014-10-09 23:52:51 +0100

    Clears screen properly

--- play.rb
+++ play.rb
@@ -1,5 +1,6 @@
 require 'io/console'
 require 'colorize'
+require 'curses'
 
 def read_char
   begin
@@ -25,20 +26,26 @@ end
 
 array = ["hello", "hi", "waddup"]
 
+# log = `git log --pretty=oneline`
+
+# array = log.split("\n")
+
 selection = 0
 
 result = []
 
 loop do
-	puts "\e[H\e[2J"
+	puts "\033c"
 	array.map! { |i| i == array[selection] ? i.green : i.black }
 	puts array
 	c = read_char
 	case c
 	when "\e[A"
 		selection -= 1 unless selection == 0
+		`clear`
 	when "\e[B"
 		selection += 1 unless selection == (array.length - 1)
+		`clear`
 	when "\r"
 		result << array[selection]
 		array.delete array[selection]
