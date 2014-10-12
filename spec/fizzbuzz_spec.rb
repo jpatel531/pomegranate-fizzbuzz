799fd4fa95d5e67918a3ee8ee045cd67f9c1bfeb
jpatel531 <jamie@notespublication.com>
2014-10-12 22:59:37 +0100

    Changed div by 3 test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,7 @@ require 'fizzbuzz'
 describe 'Fizzbuzz' do 
 
 	it 'knows when a number is divisible by 3' do 
-		expect(divisible_by_three? 3).to be_true
+		expect(divisible_by_three? 3).to be true
 	end
 
 end
